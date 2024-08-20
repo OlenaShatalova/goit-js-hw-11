921431ad1948c31785251a584f244e8b28c9afc5
Olena Shatalova <164022674+OlenaShatalova@users.noreply.github.com>
2024-08-20 12:27:48 +0300

    add-a-check-on-empty-input

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import iziToast from 'izitoast';
 import { createGalleryCardTemplate } from './js/render-functions.js';
 import { fetchPhotos } from './js/pixabay-api.js';
-import icon from '/src/img/icon.svg';
+import icon from '../src/img/icon.svg';
 
 const searchFormEl = document.querySelector('.js-search-form');
 const galleryEl = document.querySelector('.js-gallery');
@@ -10,10 +10,22 @@ const galleryEl = document.querySelector('.js-gallery');
 const onSearchFormSubmit = event => {
   event.preventDefault();
 
+  const searchedValue = searchFormEl.elements.user_query.value.trim();
+
+  if (!searchedValue) {
+    iziToast.show({
+      timeout: 5000,
+      message: 'To search, fill out the form!',
+      position: 'topLeft',
+      iconUrl: icon,
+    });
+    galleryEl.innerHTML = '';
+    searchFormEl.reset();
+    return;
+  }
+
   const loader = document.querySelector('.js-loader');
   loader.classList.remove('is-hidden');
-
-  const searchedValue = searchFormEl.elements.user_query.value;
 
   fetchPhotos(searchedValue)
     .then(imgData => {
